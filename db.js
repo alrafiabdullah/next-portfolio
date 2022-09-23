17128d4ee1b69b5e8a1824da4d7d466dcb48d1f1
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-09-23 21:34:30 +0600

    access, secret added

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 import { DynamoDB, DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 export const client = new DynamoDBClient({
-    // accessKeyId: process.env.DB_ACCESS_KEY_ID,
-    // secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.DB_ACCESS_KEY_ID,
+    secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
     region: process.env.DB_REGION
 });
 
