0d212c718f77cb185346880b1c9a986327c84ae8
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-09-23 21:25:18 +0600

    like unlike added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,38 @@
+import { Toaster, toast } from "react-hot-toast";
+import { BiLike, BiDislike } from "react-icons/bi";
+
 const Home = ({ post }) => {
+  const likeClickHandler = () => {
+    toast.success("Liked!", {
+      icon: <BiLike />,
+      style: {
+        border: "1px solid #713200",
+        // padding: "16px",
+        color: "green",
+      },
+    });
+  };
+
+  const unLikeClickHandler = () => {
+    toast.success("Unliked!", {
+      icon: <BiDislike />,
+      style: {
+        border: "1px solid #713200",
+        // padding: "16px",
+        color: "red",
+      },
+    });
+  };
+
   return (
     <div style={{ padding: "20px" }}>
+      <Toaster />
       <h1>Blog Posts</h1>
       <hr />
       <div key={post.id.N}>
         <h1>{post.title.S}</h1>
         <p>{post.content.S}</p>
+        <span><BiLike style={{ color: "blue", cursor: "pointer" }} onClick={likeClickHandler} /> <BiDislike style={{ color: "red", cursor: "pointer" }} onClick={unLikeClickHandler} /></span>
       </div>
       <hr />
     </div>
