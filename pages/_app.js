e675882955c7eb6c3b7ba44f0729240ded96638a
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-09-24 04:05:55 +0600

    base head added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import axios from "axios";
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // axios.defaults.baseURL = process.env.NODE_ENV === "production" ? process.env.PROD_URL : process.env.LOCAL_URL;
+
+  return <>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Portfolio website of Abdullah Al Rafi, Python & JS Developer, Junior Data Scientist at MedAI" />
+      <title>Abdullah Al Rafi</title>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
-export default MyApp
+export default MyApp;
