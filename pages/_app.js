b32975138e5b7494972ed408793752ec92c993d0
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-09-26 20:26:53 +0600

    meta updated

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,13 @@ function MyApp({ Component, pageProps }) {
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="keywords"
+        content="Abdullah Al Rafi, Portfolio, Python, JavaScript, Data Scientist, Django, Bangladesh, Dhaka"
+      />
+      <meta name="author" content="Abdullah Al Rafi" />
+      <meta name="creator" content="Abdullah Al Rafi" />
+      <meta name="publisher" content="Abdullah Al Rafi" />
       <meta name="description" content="Portfolio website of Abdullah Al Rafi, Python & JS Developer, Junior Data Scientist at MedAI" />
       <title>Abdullah Al Rafi</title>
     </Head>
