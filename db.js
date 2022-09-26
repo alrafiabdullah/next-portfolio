28a8a46a91dff5aa6a43e71af2d7e5adab56fe34
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-09-26 19:09:40 +0600

    firebase added

--- db.js
+++ db.js
@@ -1,16 +1,17 @@
-import { DynamoDB, DynamoDBClient } from '@aws-sdk/client-dynamodb';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const client = new DynamoDBClient({
-    accessKeyId: process.env.DB_ACCESS_KEY_ID,
-    secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
-    region: process.env.DB_REGION
-});
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
-export const db = new DynamoDB({
-    accessKeyId: process.env.DB_ACCESS_KEY_ID,
-    secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
-    region: process.env.DB_REGION
-});
-
-
-// export default client;
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 
+import { app } from "../db";
+
 const Home = () => {
+  console.log(app);
   return (
     <>
       <div style={{ textAlign: "center", marginTop: "20%" }}>
