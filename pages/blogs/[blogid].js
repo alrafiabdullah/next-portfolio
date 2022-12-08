dc5d5746d23a6bbdbae533a9fff8e345accf5db2
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-12-08 15:48:25 +0600

    implemented new backend

--- pages/blogs/[blogid].js
+++ pages/blogs/[blogid].js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Head from "next/head";
 import { useState } from "react";
 import { HashLoader } from "react-spinners";
 
@@ -12,8 +11,8 @@ const BlogPost = ({ post }) => {
 
     return (
         <div style={{ textAlign: "center" }}>
-            <h1>Blog Title: {post.data.title}</h1>
-            <p style={{ border: "1px solid black", padding: "20px", marginRight: "100px", marginLeft: "100px" }}>{post.data.content}</p>
+            <h1>Blog Title: {post.title}</h1>
+            <p style={{ border: "1px solid black", padding: "20px", marginRight: "100px", marginLeft: "100px" }}>{post.body}</p>
             <div style={{
                 marginBottom: "20px",
                 display: "flex",
@@ -30,20 +29,24 @@ export const getStaticProps = async (context) => {
     const { blogid } = context.params;
 
     // split string by - and take the last item
-    const id = blogid.split("-").pop();
+    const idArr = blogid.split("-");
+    const id = idArr.slice(-5).join("-");
+
 
     let url = process.env.NEXT_PUBLIC_LOCAL_URL;
     if (process.env.NODE_ENV === "production") {
         url = process.env.NEXT_PUBLIC_PROD_URL;
     }
 
-    const res = await fetch(`${url}/api/posts/${id}`);
-    const post = await res.json();
+    const res = await fetch(`${url}/blog/`);
+    const posts = await res.json();
+
+    const post = posts.find((post) => post.id === id);
 
     return {
         props: {
             post: post,
-            title: post.data.title,
+            title: post.title,
         },
 
         revalidate: 10,
@@ -56,11 +59,11 @@ export const getStaticPaths = async () => {
         url = process.env.NEXT_PUBLIC_PROD_URL;
     }
 
-    const res = await fetch(`${url}/api/posts`);
+    const res = await fetch(`${url}/blog/`);
     const posts = await res.json();
 
     const paths = posts.map((post) => ({
-        params: { blogid: `${post.data.slug}-${post.id}` },
+        params: { blogid: `${post.slug}-${post.id}` },
     }));
 
     return {
