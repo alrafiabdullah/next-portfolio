57d9579b22cb93f392d8b5d95d7ead06a1480f17
alrafiabdullah <abdullah34alrafi@gmail.com>
2026-02-06 20:44:34 +0100

    Fix old codes

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-import Error from "next/error";
-
-const Custom404 = () => {
-    return <>
-        <Error statusCode={404} title="You are fishing in the wrong pond" />
-    </>;
-};
-
-export default Custom404;
